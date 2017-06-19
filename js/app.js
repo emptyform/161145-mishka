e6283a2abcf01a91174a7dd103510731f34f0441
bmind12 <ryabovs@yahoo.com>
2017-06-19 17:07:01 +0200

    Progressive enhancment fur main menu on mobile version.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ var toggler = document.querySelector('.main-nav__toggler');
 var navigation = document.querySelector('.main-nav__navigation-list');
 var userPanel = document.querySelector('.main-nav__user-panel-list');
 
+navigation.classList.add('main-nav__navigation-list--close');
+userPanel.classList.add('main-nav__user-panel-list--close');
+toggler.classList.remove('main-nav__toggler--hidden');
+toggler.classList.add('main-nav__toggler--close');
+
 toggler.addEventListener('click', function () {
   navigation.classList.toggle('main-nav__navigation-list--close');
   userPanel.classList.toggle('main-nav__user-panel-list--close');
