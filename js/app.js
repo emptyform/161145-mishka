6e37b303d0cf08298a37190a64c54dc9f385d906
bmind12 <ryabovs@yahoo.com>
2017-06-25 15:06:45 +0200

    Map image implemented

--- js/app.js
+++ js/app.js
@@ -6,11 +6,15 @@ var userPanel = document.querySelector('.main-nav__user-panel-list');
 var itemCart = document.querySelectorAll('.catalog-item__cart');
 var itemModule = document.querySelector('.item-module');
 var orderBtn = document.querySelector('.week-deal__order');
+var mapImage = document.querySelector('.contacts__map--image');
+var mapIframe = document.querySelector('.contacts__map--iframe');
 
 navigation.classList.add('main-nav__navigation-list--close');
 userPanel.classList.add('main-nav__user-panel-list--close');
 toggler.classList.remove('main-nav__toggler--hidden');
 toggler.classList.add('main-nav__toggler--close');
+mapImage.classList.add('contacts__map--hidden');
+mapIframe.classList.remove('contacts__map--hidden');
 
 toggler.addEventListener('click', function () {
   navigation.classList.toggle('main-nav__navigation-list--close');
